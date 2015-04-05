d580e1c169994c2f4e33908e1cee2a73146ea684
Maverick JS <mavjs01@gmail.com>
2015-04-06 00:35:41 +0200

    removed bank codes into banks.go

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/cli"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 )
 
@@ -37,39 +33,4 @@ func main() {
 	}
 
 	app.Run(os.Args)
-}
-
-func cbm() string {
-	var err error
-
-	// request http api
-	res, err := http.Get("http://forex.cbm.gov.mm/api/latest")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// read body
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// parse json
-	type currency struct {
-		USD string
-		EUR string
-	}
-
-	type jsonUser struct {
-		Rates currency
-	}
-	user := jsonUser{}
-
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return fmt.Sprintf("USD: %s\nEUR: %s", user.Rates.USD, user.Rates.EUR)
 }
